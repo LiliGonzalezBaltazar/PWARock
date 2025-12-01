4e6dc0de45732eff82a9bbfb4464bd7564f69a0a
IZEICG-GabrielGarcia <ggarcia@izeicg.com>
2025-11-30 22:19:33 -0600

    correccion pwa

--- main.js
+++ main.js
@@ -10,3 +10,13 @@ $(document).ready(function () {
         return false;
     });
 });
+
+if('serviceWorker' in navigator){
+    console.log('Puedes usar los serviceWorker del navegador');
+
+    navigator.serviceWorker.register('./sw.js')
+        .then(res => console.log('serviceWorker cargado correctamente', res))
+        .catch(err => console.log('serviceWorker no se ha podido registrar', err))
+}else{
+    console.log('NO puedes usar los serviceWorker del navegador');
+}
