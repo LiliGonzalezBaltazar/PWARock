df9742ea7dbba61234cc1e3744e979e8a1e8b82b
IZEICG-GabrielGarcia <ggarcia@izeicg.com>
2025-11-30 21:38:30 -0600

    correcciones

--- main.js
+++ main.js
@@ -10,11 +10,3 @@ $(document).ready(function () {
         return false;
     });
 });
-
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-        navigator.serviceWorker.register("/sw.js")
-            .then(reg => console.log("SW registrado:", reg.scope))
-            .catch(err => console.error("Error al registrar SW:", err));
-    });
-}
